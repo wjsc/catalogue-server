677efeb2085708cadb79cefd5e73816288b4d35c
wjsc <elballetdelosimbeciles@gmail.com>
2017-12-09 10:22:15 -0300

    Docker, Docker-compose & Mongo:3.4.0

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/www/catalogue_api/
 npm install
-pm2 start -x ./src/index.js --no-daemon --watch "./"
+pm2 --name catalogue_api start -x src/index.js --no-daemon --watch "./src"
 
