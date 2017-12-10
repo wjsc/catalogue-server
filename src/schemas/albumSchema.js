b1da5a8f55052736c5a84ccbe1b509462ee87ca4
wjsc <walterscarano@outlook.com>
2017-12-10 11:36:08 -0300

    Schema fixs

--- src/schemas/albumSchema.js
+++ src/schemas/albumSchema.js
@@ -2,9 +2,9 @@ const Joi = require('joi');
 const mongoIdSchema = require( __dirname+'/mongoIdSchema.js');
 
 const albumSchema = Joi.object().keys({
-	title: Joi.string().alphanum().min(3).required(),
+	title: Joi.string().min(3).required(),
 	year: Joi.number().integer().min(1900).max((new Date()).getFullYear()).required(),
-	cover: Joi.string().alphanum().required(),
+	cover: Joi.string().required(),
 	tracks: Joi.array().items(mongoIdSchema)
 });
 
--- src/schemas/artistSchema.js
+++ src/schemas/artistSchema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 const mongoIdSchema = require( __dirname+'/mongoIdSchema.js');
 
 const artistSchema = Joi.object().keys({
-	name: Joi.string().alphanum().min(3).required(),
+	name: Joi.string().min(3).required(),
 	albums: Joi.array().items(mongoIdSchema)
 });
 
