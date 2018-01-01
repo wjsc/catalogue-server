54e9769a448bb597b15a572f4f60fc371ccd6d9d
wjsc <walterscarano@outlook.com>
2018-01-01 12:52:23 -0300

    date schema in history

--- src/schemas/historySchema.js
+++ src/schemas/historySchema.js
@@ -3,7 +3,8 @@ const mongoIdSchema = require( __dirname+'/mongoIdSchema.js');
 
 const historySchema = Joi.object().keys({
 	user: mongoIdSchema,
-	track: mongoIdSchema
+	track: mongoIdSchema,
+	date: Joi.date().iso()
 });
 
 module.exports = historySchema;
